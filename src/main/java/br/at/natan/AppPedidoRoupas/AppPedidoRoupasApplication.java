9493b96a04e06da2614457afc50caa7be2c045ad
natan <natanmelo5@hotmail.com>
2021-12-02 15:05:00 -0500

    Initial commit

---
+++ src/main/java/br/at/natan/AppPedidoRoupas/AppPedidoRoupasApplication.java
@@ -0,0 +1,13 @@
+package br.at.natan.AppPedidoRoupas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPedidoRoupasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPedidoRoupasApplication.class, args);
+	}
+
+}
